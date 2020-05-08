dd936caad86ef04d705c7e1620aa84694a49e0f3
1119864054 <1119864054@qq.com>
2020-05-08 12:44:50 +0800

    基础框架 20200508

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -13,9 +13,19 @@ class TestSample:
 
     @pytest.fixture(autouse=True, scope='function')
     def set_up(self, driver):
-        driver.get('https://pypi.org/')
+        url = 'https://pypi.org/'
+        driver.get(url)
+        logger.info('goto url %s' % url)
 
     def test_1(self, driver):
+        """
+        1.基础方法封装(base_action.py)
+        2.页面对象(page_object/)
+        3.页面操作(test_*.py)
+
+        :param driver:
+        :return:
+        """
         page_pypi = PagePIPY(driver)
         title = page_pypi.get_banner()
         print(title)
